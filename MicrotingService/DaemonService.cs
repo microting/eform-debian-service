d3ea7edc1d36488766425e310066f03deb7ae8f7
René Schultz Madsen <rm@microting.dk>
2023-11-25 07:27:36 +0100

    Adding Sentry error logging.

--- MicrotingService/DaemonService.cs
+++ MicrotingService/DaemonService.cs
@@ -1,11 +1,13 @@
 using System;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Sentry;
 
 namespace MicrotingService
 {
@@ -45,6 +47,26 @@ namespace MicrotingService
                 Console.WriteLine("Using connection string from config");
                 Console.WriteLine($"Connection string: {_config.Value.ConnectionString}");
                 serverConnectionString = _config.Value.ConnectionString;
+            }
+
+            string pattern = @"Database=(\d+)_Angular;";
+            Match match = Regex.Match(serverConnectionString!, pattern);
+
+            if (match.Success)
+            {
+                string numberString = match.Groups[1].Value;
+                int number = int.Parse(numberString);
+                SentrySdk.ConfigureScope(scope =>
+                {
+                    scope.SetTag("customerNo", number.ToString());
+                    Console.WriteLine("customerNo: " + number);
+                    scope.SetTag("osVersion", Environment.OSVersion.ToString());
+                    Console.WriteLine("osVersion: " + Environment.OSVersion);
+                    scope.SetTag("osArchitecture", RuntimeInformation.OSArchitecture.ToString());
+                    Console.WriteLine("osArchitecture: " + RuntimeInformation.OSArchitecture);
+                    scope.SetTag("osName", RuntimeInformation.OSDescription);
+                    Console.WriteLine("osName: " + RuntimeInformation.OSDescription);
+                });
             }
 
             _serviceLogic = new ServiceLogic();
--- MicrotingService/Program.cs
+++ MicrotingService/Program.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Sentry;
 
 namespace MicrotingService
 {
@@ -12,6 +13,30 @@ namespace MicrotingService
     {
         static async Task Main(string[] args)
         {
+            SentrySdk.Init(options =>
+            {
+                // A Sentry Data Source Name (DSN) is required.
+                // See https://docs.sentry.io/product/sentry-basics/dsn-explainer/
+                // You can set it in the SENTRY_DSN environment variable, or you can set it in code here.
+                options.Dsn = "https://2778f21466bfd0220a0cd1c928b2738c@o4506241219428352.ingest.sentry.io/4506285004685312";
+
+                // When debug is enabled, the Sentry client will emit detailed debugging information to the console.
+                // This might be helpful, or might interfere with the normal operation of your application.
+                // We enable it here for demonstration purposes when first trying Sentry.
+                // You shouldn't do this in your applications unless you're troubleshooting issues with Sentry.
+                options.Debug = false;
+
+                // This option is recommended. It enables Sentry's "Release Health" feature.
+                options.AutoSessionTracking = true;
+
+                // This option is recommended for client applications only. It ensures all threads use the same global scope.
+                // If you're writing a background service of any kind, you should remove this.
+                options.IsGlobalModeEnabled = false;
+
+                // This option will enable Sentry's tracing features. You still need to start transactions and spans.
+                options.EnableTracing = true;
+            });
+
             var builder = new HostBuilder()
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
